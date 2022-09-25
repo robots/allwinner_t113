930a715630485ddc87331870cd5933ca054c10f8
Michal Demin <michaldemin@gmail.com>
2022-09-25 20:29:47 +0200

    add missing event.h
    Signed-off-by: Michal Demin <michaldemin@gmail.com>

---
+++ fw_lite/event.h
@@ -0,0 +1,17 @@
+#ifndef EVENT_h_
+#define EVENT_h_
+
+#include "FreeRTOS.h"
+#include "queue.h"
+
+typedef struct {
+	uint8_t is_modifier;
+	uint8_t down;
+	uint8_t code;
+	uint8_t ch;
+} kbd_event_t;
+
+extern QueueHandle_t kbd_queue;
+
+
+#endif
